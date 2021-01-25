168217f88a6fa77bd6aaac07763b4bec299ba1d0
Bert Bruynooghe <bert@bruynooghe-polet.com>
2021-01-25 08:04:44 +0100

    refactor

--- test.mjs
+++ test.mjs
@@ -5,14 +5,14 @@ const findRootElement = el => {
   return findRootElement(el.parentElement)
 }
 
-window.startMessage = rootElement => { 
+window.StartMessageController = rootElement => { 
   startMessageRoots.push(rootElement || event.target.parentElement)
   alert('connect')
 
   const clickHandler = () => {
     const myRoot = findRootElement(event.target)
     // TODO: ignore the targets of nested controllers
-    // can probably partly be mitigated by looking for [data-target="theTarget: startMessage"],
+    // can probably partly be mitigated by looking for [data-target="theTarget: StartMessageController"],
     // but that doesn't solve nesting of same things
     // so we probably should ignore duplicate entries from a deeper layer.
     // (not: first one wins) 
@@ -23,14 +23,6 @@ window.startMessage = rootElement => {
     targets.theTarget.remove()
   } 
   
-  Object.assign(window.startMessage, { clickHandler })
-  return window.startMessage
-}
-
-// the controller
-const StartMessageController = rootElement => {
-  alert('connected')
-  return {
-    clickHandler: (targets, e) => targets.theTarget.remove
-  }
+  Object.assign(window.StartMessageController, { clickHandler })
+  return window.StartMessageController
 }
