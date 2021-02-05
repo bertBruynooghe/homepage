0ecbf4ff7e1c66b8caeccff4527216e85961e5c8
Bert Bruynooghe <bert@bruynooghe-polet.com>
2021-02-05 09:52:15 +0100

    refactor

--- stimulusP/controllersLoaderWithEvents.mjs
+++ stimulusP/controllersLoaderWithEvents.mjs
@@ -1,11 +1,11 @@
 const ctrlAtt = 'data-controller'
 
-const getRoot = 
-  el => el.getAttribute(ctrlAtt) ? el : getRoot(el.parentElement)
+const ctrlName = el => el.getAttribute(ctrlAtt)
+const getRoot = el => ctrlName(el) ? el : getRoot(el.parentElement)
 
 window.handleBy = 
   ([methodName]) => getRoot(window.event.currentTarget)
     .controller[methodName](window.event)
 
 for (const el of document.querySelectorAll(`[${ctrlAtt}]`))
-  el.controller = controllers[el.getAttribute(ctrlAtt)](el)
+  el.controller = controllers[ctrlName(el)](el)
--- stimulusP/controllersLoaderWithTargetedEvents.mjs
+++ stimulusP/controllersLoaderWithTargetedEvents.mjs
@@ -2,14 +2,14 @@ const ctrlAtt = 'data-controller'
 const targetAtt = 'data-target'
 const targetSelector = `[${targetAtt}], :not([${ctrlAtt}] *)[${targetAtt}]`
 
-const getRoot = 
-  el => el.getAttribute(ctrlAtt) ? el : getRoot(el.parentElement)
-
-const getTargetsObject = el => 		
-  el.querySelectorAll(targetSelector).reduce(targetElementReducer, {})
+const ctrlName = el => el.getAttribute(ctrlAtt)
+const getRoot = el => ctrlName(el) ? el : getRoot(el.parentElement)
 
 const targetElementReducer = (acc, el) => 
   ({ ...acc, [el.getAttribute(targetAtt)] : el })
+
+const getTargetsObject = el => 		
+  [...el.querySelectorAll(targetSelector)].reduce(targetElementReducer, {})
 
 window.handleBy = ([methodName]) => {
   const root = getRoot(window.event.currentTarget)
@@ -17,4 +17,4 @@ window.handleBy = ([methodName]) => {
 }
 
 for (const el of document.querySelectorAll(`[${ctrlAtt}]`))
-  el.controller = controllers[el.getAttribute(ctrlAtt)](el)
+  el.controller = controllers[ctrlName(el)](el)
