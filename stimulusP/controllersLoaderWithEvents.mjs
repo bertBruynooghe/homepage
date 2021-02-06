1e23618f340a8bf7555e17484006103f47203909
Bert Bruynooghe <bert@bruynooghe-polet.com>
2021-02-06 10:45:25 +0100

    removed console.log statements

--- stimulusP/controllersLoaderWithEvents.mjs
+++ stimulusP/controllersLoaderWithEvents.mjs
@@ -5,13 +5,8 @@ const getRoot =
   ({ parentElement: el }) => ctrlName(el) ? el : getRoot(el.parentElement)
 
 window.handleBy = 
-  ([methodName]) => {
-    console.log({ methodName })
-    getRoot(window.event.currentTarget)
+  ([methodName]) => getRoot(window.event.currentTarget)
     .controller[methodName](window.event)
-  }
 
-for (const el of document.querySelectorAll(`[${ctrlAtt}]`)){
-  console.log('loaded controller', ctrlName(el))
+for (const el of document.querySelectorAll(`[${ctrlAtt}]`))
   el.controller = controllers[ctrlName(el)](el)
-}
