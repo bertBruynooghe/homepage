d1ac1eb2ab2b2b020e7f22a9907ecf610680feb9
Bert Bruynooghe <bert@bruynooghe-polet.com>
2021-02-09 20:11:47 +0100

    fix #7: prevent multple parsing of targets

--- stimulusP/v2/controllersLoaderWithTargetListEvents.mjs
+++ stimulusP/v2/controllersLoaderWithTargetListEvents.mjs
@@ -4,22 +4,26 @@ const actionSelector = `:scope > [${actionAtt}], :not([${ctrlAtt}] *)[${actionAt
 const targetSelector = `:scope > [${targetAtt}], :not([${ctrlAtt}] *)[${targetAtt}]`
   
 const ctrlName = root => root.getAttribute(ctrlAtt)
-const controller = root => controllers[ctrlName(root)](root, targets(root))
 const tupleReducer = (object, [k,v]) => 
   ({ ...object, [k]: object[k] ? [].concat(object[k], v) : v })
 const targetElementReducer = (targets, target) =>
   tupleReducer(targets, [target.getAttribute(targetAtt), target])
 const targets = root => 		
   [...root.querySelectorAll(targetSelector)].reduce(targetElementReducer, {})
-const eventHandlers = (root, actionable) => {
+
+const eventHandlers = (controller, actionable) => {
   const handlerTuplesString = actionable.getAttribute(actionAtt)
   const expr = `with(arguments[0]){ return { ${handlerTuplesString} } }`
-  return (Function(expr))(controller(root, targets(root)))
+  return (Function(expr))(controller)
 }
 
-for (const root of document.querySelectorAll(`[${ctrlAtt}]`)){
-  controllers[ctrlName(root)](root, targets(root))
+const parseController = (root, controller, targets) => {
   for (const actionable of root.querySelectorAll(actionSelector))
-    for (const [k,v] of Object.entries(eventHandlers(root, actionable)))
-      actionable.addEventListener(k, e => v(e, targets(root)))
+    for (const [k,v] of Object.entries(eventHandlers(controller, actionable)))
+      actionable.addEventListener(k, e => v(e, targets))
 }
+const initControllerWithTargets = (root, targets) =>
+  parseController(root, controllers[ctrlName(root)](root, targets), targets)
+const initController = root => initControllerWithTargets(root, targets(root))
+
+for (const el of document.querySelectorAll(`[${ctrlAtt}]`)) initController(el)
